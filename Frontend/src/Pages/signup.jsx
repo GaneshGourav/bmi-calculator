53beaf4de5b8e1b6f3b895838a11ee4a2012437a
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-03 22:11:41 +0530

    signup added

---
+++ Frontend/src/Pages/signup.jsx
@@ -0,0 +1,17 @@
+import { Container, Text } from "@chakra-ui/react"
+
+export const Signup = () =>{
+
+    return <>
+    
+    <Container>
+        <Text>Signup here !</Text>
+
+
+    </Container>
+    
+    
+    </>
+
+
+}
