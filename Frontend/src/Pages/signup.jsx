1d521a731871295aafaf61fbf7af4ea935483ed8
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-05 21:26:00 +0530

    signup deisgn added

--- Frontend/src/Pages/signup.jsx
+++ Frontend/src/Pages/signup.jsx
@@ -1,22 +1,60 @@
-import { Button, Container, Input, Text } from "@chakra-ui/react"
+import { Button, Container, Input, Text } from "@chakra-ui/react";
 
-export const Signup = () =>{
-
-    return <>
-    
-    <Container>
-        <Text color={"teal"} fontSize={"bold"} fontFamily={"cursive"} marginTop={"30px"} marginBottom={"30px"}>Signup here !</Text>
-        <Input type="text" placeholder="username" border={"1px solid teal"} autoCorrect="on" spellCheck /> <br /><br />
-        <Input type="email" placeholder="email"  border={"1px solid teal"} autoCorrect="on" spellCheck /><br /><br />
-        <Input type="password" placeholder="password"  border={"1px solid teal"} autoCorrect="on" spellCheck /><br /><br /><br />
-        <Button colorScheme="teal" width={"100%"}>SignUp</Button>
-
-
-    </Container>
-   
-    
-    
+export const Signup = () => {
+  return (
+    <>
+      <Container>
+        <Text
+          color={"teal"}
+          fontSize={"bold"}
+          fontFamily={"cursive"}
+          marginTop={"30px"}
+          marginBottom={"30px"}
+        >
+          Signup here !
+        </Text>
+        <Input
+          type="text"
+          placeholder="username"
+          border={"1px solid teal"}
+          autoCorrect="on"
+          spellCheck
+        />{" "}
+        <br />
+        <br />
+        <Input
+          type="email"
+          placeholder="email"
+          border={"1px solid teal"}
+          autoCorrect="on"
+          spellCheck
+        />
+        <br />
+        <br />
+        <Input
+          type="password"
+          placeholder="password"
+          border={"1px solid teal"}
+          autoCorrect="on"
+          spellCheck
+        />
+        <br />
+        <br />
+        <br />
+        <Button colorScheme="teal" width={"100%"}>
+          SignUp
+        </Button>
+        <br />
+        <br />
+        <Text>
+          If you are already registered{" "}
+          <span
+            style={{ color: "teal", fontWeight: "bold", fontFamily: "cursive" }}
+          >
+            login here
+          </span>{" "}
+        </Text>
+      </Container>
     </>
-
-
-}
+  );
+};
