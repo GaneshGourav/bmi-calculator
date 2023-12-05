33c3a4996e6b870845b0685c9f326a547e96987f
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-05 21:29:01 +0530

    login style

--- Frontend/src/Pages/login.jsx
+++ Frontend/src/Pages/login.jsx
@@ -3,7 +3,11 @@ import { Container, Input, Text } from "@chakra-ui/react"
 export const Login = () =>{
     return<>
     <Container>
-        <Text>Login here !</Text>
+        <Text  color={"teal"}
+          fontWeight={"bold"}
+          fontFamily={"cursive"}
+          marginTop={"30px"}
+          marginBottom={"30px"}>Login here !</Text>
         <Input type="email" placeholder="email" autoCorrect="on" spellCheck />
         <Input type="password" placeholder="password" autoCorrect="on" spellCheck />
     </Container>
