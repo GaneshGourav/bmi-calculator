7ce9f230fbd960fee5e34255db1447178f2f9491
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-06 11:49:02 +0530

    clculator added

--- Frontend/src/Pages/bmicalculate.jsx
+++ Frontend/src/Pages/bmicalculate.jsx
@@ -4,14 +4,14 @@ export const BmiCalculate = () =>{
 
 
     return <>
-    <div style={{display:"flex",justifyContent:"space-evenly",alignItems:"center"}}>
-        <div style={{border:"1px solid red"}}>
+    <div style={{display:"flex",justifyContent:"space-evenly",alignItems:"center",marginTop:"30px"}}>
+        <div style={{border:"1px solid red",padding:"50px"}}>
             <Text color={"teal"} fontFamily={"cursive"} fontWeight={"bold"}>Calculate Your Body Mass Index</Text>
-<Container>
+<Container marginTop={"30px"} padding={"30px"} width={"100%"}>
     <form >
-    <Input type="number" placeholder={"Enter your Height in feet"} />
-    <Input type="number" placeholder={"Enter your weight in Kgs"} />
-    <Button colorScheme="teal" fontFamily={"cursive"} fontWeight={"bold"}>Calculate BMI</Button>
+    <Input type="number" placeholder={"Enter your Height in feet"} /> <br /><br />
+    <Input type="number" placeholder={"Enter your weight in Kgs"} /> <br /><br />
+    <Button colorScheme="teal" fontFamily={"cursive"} fontWeight={"bold"} width={"100%"}>Calculate BMI</Button>
     </form>
 
 </Container>
