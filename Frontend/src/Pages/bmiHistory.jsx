4d32e5a6120434e05c6d220e252c21c62a477f42
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-09 22:03:13 +0530

    table added in history page

--- Frontend/src/Pages/bmiHistory.jsx
+++ Frontend/src/Pages/bmiHistory.jsx
@@ -1,12 +1,63 @@
-export const BmiHostory = () =>{
-    return <>
-    <table>
-        <thead>
-            <th>Name</th>
-            <th>BMI</th>
-            <th>Status</th>
-        </thead>
-    </table>
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getData } from "../redux/Authentication/action";
+import {
+  GET_DATA_ERROR,
+  GET_DATA_SUCCESS,
+} from "../redux/Authentication/actionTypes";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+export const BmiHostory = () => {
+  const [data, setData] = useState([]);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getData())
+      .then((res) => {
+        dispatch({ type: GET_DATA_SUCCESS, data: res.data });
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        dispatch({ type: GET_DATA_ERROR });
+        console.log(err);
+      });
+  }, []);
+  console.log(data);
+
+  return (
+    <>
+     <TableContainer>
+  <Table variant='striped' colorScheme='teal'>
+    <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>S.I</Th>
+        <Th>BMI</Th>
+        <Th>Status</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+   
+    </Tbody>
     
+  </Table>
+</TableContainer>
     </>
-}
+  );
+};
--- Frontend/src/components/MainRouter.jsx
+++ Frontend/src/components/MainRouter.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Signup } from "../Pages/signup";
 import { Login } from "../Pages/login";
 import { BmiCalculate } from "../Pages/bmicalculate";
+import { BmiHostory } from "../Pages/bmiHistory";
 export const MainRoutes = () => {
   return (
     <>
@@ -9,7 +10,7 @@ export const MainRoutes = () => {
         <Route path="/bmi-calculation" element={<BmiCalculate/>} />
         <Route path="/account-signin" element={<Signup />} />
         <Route path="/account-login" element={<Login />} />
-        <Route path="/bmi-history" element={<Login />} />
+        <Route path="/bmi-history" element={<BmiHostory />} />
 
       </Routes>
     </>
